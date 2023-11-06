58afa04ebcf189079dcc20fb978b16b08c64eec4
Romain Brosolo <79928407+RomainBrosolo@users.noreply.github.com>
2023-11-06 16:59:24 +0100

    design card

--- src/app/home/components/dashboard/item-event/item-event.component.ts
+++ src/app/home/components/dashboard/item-event/item-event.component.ts
@@ -4,7 +4,7 @@ import { Evenement } from 'src/app/core/models/event.model';
 @Component({
   selector: 'app-item-event',
   templateUrl: './item-event.component.html',
-  styleUrls: ['./item-event.component.css']
+  styleUrls: ['./item-event.component.scss']
 })
 export class ItemEventComponent {
 
