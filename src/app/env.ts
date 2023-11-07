c396d8eaf278dc1e6df4d0383c8f0bcba9200292
unes <64701810+prodamex@users.noreply.github.com>
2023-11-07 22:33:17 +0100

    Base de données créé et FireBase connecté

---
+++ src/app/env.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDrlsOJQTjnborEqbuT-ewIcT5ODLfIu4M",
+    authDomain: "eventi-179d1.firebaseapp.com",
+    projectId: "eventi-179d1",
+    storageBucket: "eventi-179d1.appspot.com",
+    messagingSenderId: "818081271611",
+    appId: "1:818081271611:web:7e698086492a8dce40f11e"
+  };
