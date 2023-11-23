363c3eb8d8feb1326d06510762adf475d8755e50
AnassAoulad <anass.aoulad.bh@gmail.com>
2023-11-23 22:26:43 +0100

    [Create] Adding authentification

--- src/app/shared/vendors/material.module.ts
+++ src/app/shared/vendors/material.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field'; 
-import {MatSelectModule} from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-const materialModules: any[] = [MatFormFieldModule, MatSelectModule
-  ];
+const materialModules: any[] = [MatFormFieldModule, MatSelectModule, MatToolbarModule];
 
 @NgModule({
-    imports: [...materialModules],
-    exports: [...materialModules],
-  })
+  imports: [...materialModules],
+  exports: [...materialModules],
+})
 
-  export class MaterialModule {}
+export class MaterialModule { }
